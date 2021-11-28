bc830b29abb618fe8ec175e5b97de4a5d7404f4c
piero <gagonzabe@gmail.com>
2021-11-28 11:22:38 -0500

    Agregué login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
-
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,9 @@ import { FooterComponent } from './plantillas/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    usuario : new FormControl('usuario@gmail.com', Validators.required),
+    password : new FormControl('', Validators.required)
+  }) 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(form: any){
+    console.log(form)
   }
 
 }
